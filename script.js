711bb8088d0e4c6022544380d68439d3126ac489
ABUBAKARTARSUM <80694589+ABUBAKARTARSUM@users.noreply.github.com>
2022-01-01 15:50:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const buttonShare = document.getElementById('buttonShare');
+buttonShare.onclick = function () {
+    if(!navigator.share) return
+    navigator.share({
+        title: 'BAKRI DESIGN',
+        text: 'Thanks for sharing this website',
+        url: 'https://abubakartarsum.github.io/Bakri-Blog',
+    })
+    .then(() => console.log('Successful Sharing Website'))
+    .catch((error) => console.log('Error sharing', 
+    error));
+}
